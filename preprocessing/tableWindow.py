52e5236bdf0b4c6be01960c0a52132d87a0decd8
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-01-16 14:31:04 +0100

    included agent selction

--- preprocessing/tableWindow.py
+++ preprocessing/tableWindow.py
@@ -10,6 +10,7 @@ from PyQt4 import QtGui, QtCore
 import pandas as pd
 from numpy import random
 import sys
+from agentWindow import agentWindow1
 
 
 class tableWindow(QtGui.QWidget):
@@ -23,8 +24,12 @@ class tableWindow(QtGui.QWidget):
         self.parentWindow = parentWindow
         self.fileName = fileName #name of the selected csv
         
-        self.checkLabels = {'time': 2, 'frames': 1, 'agent0_x': 5, 'agent0_y': 6, 'agent0_angle': 7, 
-                            'agent1_x': 10, 'agent1_y': 11, 'agent1_angle': 12} # labels for the selcted columns
+        self.checkLabels = {'TIME': {'time': 2, 'frames': 1}, 'AGENTS':{ 'agent0_x': 5, 'agent0_y': 6, 'agent0_angle': 7, 
+                            'agent1_x': 10, 'agent1_y': 11, 'agent1_angle': 12}} # labels for the selcted columns
+        
+        self.AGENT_NAMES = ['agent0', 'agent1']
+        self.AGENT_DATA = ['_x', '_y', '_angle']   
+        self.agentLEs = []
                             
         self.nColumns = 0 #number of columns of the original file 
 
@@ -52,9 +57,7 @@ class tableWindow(QtGui.QWidget):
         # ---------------------------------------------------------------------
         # this can be done smarter...
         # ---------------------------------------------------------------------
-        nAgents = 2
-        base = 'agent'
-        specs = ['_x', '_y', '_angle']
+
         
         #for j, key in enumerate(self.checkLabels): 
         
@@ -63,26 +66,18 @@ class tableWindow(QtGui.QWidget):
             cb.setText(key)
             
             le = QtGui.QLineEdit(self)
-            le.setText(str(self.checkLabels[key]))
+            le.setText(str(self.checkLabels['TIME'][key]))
             le.textChanged.connect(self.setColumn)
             le.setObjectName(key)
             le.setValidator(QtGui.QIntValidator())
             self.checkBoxLayout.addWidget(cb, 0, 2*j)
             self.checkBoxLayout.addWidget(le, 0, 2*j +1)
             
-        for i in range(nAgents): 
-            for k, s in enumerate(specs): 
-                label = base + str(i) + s
-                cb = QtGui.QLabel(self)
-                cb.setText(label)
-                
-                le = QtGui.QLineEdit(self)
-                le.setText(str(self.checkLabels[label]))
-                le.textChanged.connect(self.setColumn)
-                le.setObjectName(label)
-                le.setValidator(QtGui.QIntValidator())
-                self.checkBoxLayout.addWidget(cb, i+1, 2*k)
-                self.checkBoxLayout.addWidget(le, i+1, 2*k+1)
+            
+            
+        
+            
+        self.draw_agent_names()
 
 
         # ---------------------------------------------------------------------
@@ -97,6 +92,10 @@ class tableWindow(QtGui.QWidget):
         self.addParamsButton.setText("Add Parameters")
         self.addParamsButton.clicked.connect(self.addParams)
         
+        self.changeAgentsButton = QtGui.QPushButton(self)
+        self.changeAgentsButton.setText("Add/Remove Agents")
+        self.changeAgentsButton.clicked.connect(self.change_agents)
+        
 
         self.layoutVertical = QtGui.QVBoxLayout()
         
@@ -106,6 +105,7 @@ class tableWindow(QtGui.QWidget):
         
         self.lowerLayout = QtGui.QHBoxLayout()
         self.lowerLayout.addWidget(self.addParamsButton)
+        self.lowerLayout.addWidget(self.changeAgentsButton)
         self.lowerLayout.addWidget(self.saveButton)
         
         self.layoutVertical.addLayout(self.upperLayout)
@@ -122,13 +122,51 @@ class tableWindow(QtGui.QWidget):
     
         sender = self.sender()
         senderName = sender.objectName()
-        self.checkLabels[senderName] = sender.text()
+        for key in self.checkLabels.keys():
+            if senderName in self.checkLabels[key].keys(): 
+                self.checkLabels[key][senderName] = sender.text()
+    
+    def draw_agent_names(self): 
+    
+        if len(self.agentLEs) != 0:
+            for l in self.agentLEs: 
+                self.checkBoxLayout.removeWidget(l)
+                sip.delete(l) 
+            self.agentLEs = []
+        
+        AGENTS_COLUMNS = {}
+        for i in range(len(self.AGENT_NAMES)): 
+            for k, s in enumerate(self.AGENT_DATA ): 
+
+                cb = QtGui.QLabel(self.AGENT_NAMES[i] + s)
+                self.agentLEs.append(cb)
+                
+                le = QtGui.QLineEdit(self)
+                
+                #le.setText(str(self.checkLabels['AGENTS'][label]))
+                le.textChanged.connect(self.setColumn)
+                le.setObjectName(self.AGENT_NAMES[i] + s)
+                le.setValidator(QtGui.QIntValidator())
+                self.agentLEs.append(le)
+                
+                
+                self.checkBoxLayout.addWidget(cb, i+1, 2*k)
+                self.checkBoxLayout.addWidget(le, i+1, 2*k+1)
+                
+                AGENTS_COLUMNS[self.AGENT_NAMES[i] + s] = -1
+        
+        self.checkLabels['AGENTS'] = AGENTS_COLUMNS
 
         
     def save(self):     
         warning = False
         overlap = False
-        columnlist = list(self.checkLabels.values())
+        columnlist = []
+
+        for key in self.checkLabels.keys(): 
+            for v in self.checkLabels[key].values(): 
+                columnlist.append(v)
+        print(columnlist)
         
         if any(int(x) < 0 for x in columnlist):
         # check if all values were set
@@ -168,15 +206,23 @@ class tableWindow(QtGui.QWidget):
             
 
     def build_final_csv(self, fileName): 
+        header_dict ={}
+    
+        for key in self.checkLabels.keys(): 
+            for k in self.checkLabels[key].keys(): 
+                header_dict[k] = self.checkLabels[key][k]
+        
         df = pd.read_csv(fileName, header = None, sep = ';', names = [str(i) for i in range(self.nColumns)])
         
-        real_indices = [str(int(cl) -1) for cl in self.checkLabels.values()]
+        real_indices = [str(int(cl) -1) for cl in header_dict.values()]
         df_new = df.loc[:, real_indices]
-        df_new.columns = list(self.checkLabels.keys())
+        df_new.columns = list(header_dict.keys())
         df_new.to_csv(self.TMP_FILE_TITLE, sep = ',')
 
         print('temporary file saved to', self.TMP_FILE_TITLE)
         
+        
+        self.parentWindow.INFO['agent_names'] = self.AGENT_NAMES
         self.parentWindow.init_Info(self.TMP_FILE_TITLE)
         self.home.close()
         
@@ -186,10 +232,12 @@ class tableWindow(QtGui.QWidget):
         msg = QtGui.QMessageBox()
         msg.setIcon(QtGui.QMessageBox.Information)
         msg.setText("I can't do anything yet")
-        msg.setWindowTitle("I do nothing")
         msg.setStandardButtons(QtGui.QMessageBox.Ok)
         val = msg.exec_()
-        
+   
+    def change_agents(self): 
+        self.aw = agentWindow1(self)
+        self.aw.show()       
 
 
         
