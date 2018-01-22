5f6ae3a677e42ad0fb5a9744068fd27d6815a0d4
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-01-22 18:30:14 +0100

    incorporate default settings

--- preprocessing/tableWindow.py
+++ preprocessing/tableWindow.py
@@ -14,6 +14,7 @@ import pandas as pd
 from numpy import random
 import sys
 from agentWindow import agentWindow
+import settings.default_params as default
 
 
 class tableWindow(QtWidgets.QWidget):
@@ -21,7 +22,7 @@ class tableWindow(QtWidgets.QWidget):
     columns (with respective names) will be saved as 'tmp.csv' to be further processed by main window'''
 
     #the name of the resulting file will be passed back to the main window            
-    TMP_FILE_TITLE = 'tmp.csv'
+    TMP_FILE_TITLE = default.tmp_file
 
     def __init__(self, parentWindow, fileName):
     #def __init__(self, fileName):
@@ -31,16 +32,20 @@ class tableWindow(QtWidgets.QWidget):
         self.parentWindow = parentWindow
         self.fileName = fileName #name of the selected csv
         
-        self.checkLabels = {'TIME': {'time': 2, 'frames': 1}, 'AGENTS':{ 'agent0_x': 5, 'agent0_y': 6, 'agent0_angle': 7, 
-                            'agent1_x': 10, 'agent1_y': 11, 'agent1_angle': 12}} # labels for the selcted columns
-        
-        self.AGENT_NAMES = ['agent0', 'agent1'] # default naming for agents
-        self.AGENT_DATA = ['_x', '_y', '_angle'] # default agent info   
+        self.AGENT_NAMES = default.agent_names #default naming for agents
+        self.AGENT_DATA = default.agent_specifications #default agent info
+        self.TIME_LABELS = default.time_labels   
         self.agentLEs = []
-                            
-        self.nColumns = 0 # number of columns of the original file 
         
-        self.DELIMINATER = ';' # default deliminator for reading
+        # the check labels dictionary contains the lables and default values for all columns to set
+        self.checkLabels = {}
+        self.update_checklabels(init = True)
+
+
+
+        self.nColumns = 0 #number of columns of the original file 
+        
+        self.DELIMINATER = default.csv_delim # default deliminator for reading
 
         self.home()
         
@@ -74,7 +79,7 @@ class tableWindow(QtWidgets.QWidget):
             self.checkBoxLayout.addWidget(le, 0, 2*j +1)
             
 
-        self.draw_agent_names() # set the agent columns
+        self.draw_agent_names(init = True) # set the agent columns
 
 
         # ---------------------------------------------------------------------
@@ -127,17 +132,18 @@ class tableWindow(QtWidgets.QWidget):
         ''' reacts to changes in the text-edit field for column selection'''  
         sender = self.sender()
         senderName = sender.objectName()
+        print(senderName)
         for key in self.checkLabels.keys():
             if senderName in self.checkLabels[key].keys(): 
                 self.checkLabels[key][senderName] = sender.text()
     
-    def draw_agent_names(self): 
+    def draw_agent_names(self, init = False): 
         ''' creates a lable and text-edit widget for each agent and agent-property (x, y, angle). Any 
         preexisting widgets are deleted. This allows to change the agent number/names dynamically. If 
         the agentWindow is not called we use the default values as specified in self.checkLabels['AGENTS']'''
         
         # this block deletes old widgets if there are any
-        if len(self.agentLEs) != 0:
+        if init == False: 
             for l in self.agentLEs: 
                 self.checkBoxLayout.removeWidget(l)
                 sip.delete(l) 
@@ -145,46 +151,38 @@ class tableWindow(QtWidgets.QWidget):
         
         # this block produces the new widgets (or default widgets when called the first time
         # widget IDs are saved in self.agentLEs
-        AGENTS_COLUMNS = {}
         for i in range(len(self.AGENT_NAMES)): 
-            for k, s in enumerate(self.AGENT_DATA ): 
+            for k, s in enumerate(self.AGENT_DATA): 
 
                 cb = QtWidgets.QLabel(self.AGENT_NAMES[i] + s)
                 self.agentLEs.append(cb)
                 
                 le = QtWidgets.QLineEdit(self)                
-                #le.setText(str(self.checkLabels['AGENTS'][label]))
+                le.setText(str(self.checkLabels['AGENTS'][self.AGENT_NAMES[i] + s]))
                 le.textChanged.connect(self.setColumn)
                 le.setObjectName(self.AGENT_NAMES[i] + s)
                 le.setValidator(QtGui.QIntValidator())
                 self.agentLEs.append(le)
                 
-                
                 self.checkBoxLayout.addWidget(cb, i+1, 2*k)
                 self.checkBoxLayout.addWidget(le, i+1, 2*k+1)
-                
-                AGENTS_COLUMNS[self.AGENT_NAMES[i] + s] = -1
-        
-        self.checkLabels['AGENTS'] = AGENTS_COLUMNS
 
+        
     def check_entries(self): 
         ''' checks if the user entries are valid and returns a boolean value'''
-        valid = True
-        
+        valid = True        
         columnlist = []# bulid a list of all columns of the final csv
+
         for key in self.checkLabels.keys(): 
             for v in self.checkLabels[key].values(): 
                 columnlist.append(int(v) -1)
          
         if any(x < 0 for x in columnlist): # check if all values were set (default = -1)
-            valid = False
-        
+            valid = False        
         elif len(columnlist) != len(set(columnlist)): # check if no value appears twice
-            valid = False
-        
+            valid = False        
         elif max(columnlist) > self.nColumns: # check if columns are in range of original file
-            valid = False
-            
+            valid = False            
         return valid
         
     def send_warning(self, text): 
@@ -195,10 +193,7 @@ class tableWindow(QtWidgets.QWidget):
         msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msg.setText(text)
         val = msg.exec_()
-            
-        
-            
-                    
+
     def save(self):     
         ''' gets called by the save Button. Checks if selected columns are valid, if so, the tmp.csv is created
         otherwise a warning is sent'''
@@ -207,7 +202,7 @@ class tableWindow(QtWidgets.QWidget):
         if valid: 
             self.build_final_csv(self.fileName)
         else: 
-            self.send_warning('Column selection invalid')
+            self.send_warning('Column selection invalid: Check for empty fields, double indices and indices exceeding size of original file')
             
             
 
@@ -226,8 +221,7 @@ class tableWindow(QtWidgets.QWidget):
         df_new.to_csv(self.TMP_FILE_TITLE, sep = ',')
 
         print('temporary file saved to', self.TMP_FILE_TITLE)
-        
-        
+  
         self.parentWindow.INFO['agent_names'] = self.AGENT_NAMES
         self.parentWindow.init_Info(self.TMP_FILE_TITLE)
         self.home.close()
@@ -245,8 +239,19 @@ class tableWindow(QtWidgets.QWidget):
     def change_agents(self): 
         '''calls the agent window which allows to set number and names of agents'''
         self.aw = agentWindow(self, self.AGENT_NAMES)
-        self.aw.show()       
+        self.aw.show()   
+        
+    def update_checklabels(self, init = False): 
+        print('checkLabels were updated')
 
+        if init == True or len(self.AGENT_NAMES)*len(self.AGENT_DATA) == len(default.agent_columns): 
+            self.checkLabels = {'TIME': {self.TIME_LABELS[i] : default.time_columns[i] for i in range(len(self.TIME_LABELS))}, 
+            'AGENTS': {self.AGENT_NAMES[i] + self.AGENT_DATA[k] : default.agent_columns[i*len(self.AGENT_DATA)+k] for k in range(len(self.AGENT_DATA)) for i in range(len(self.AGENT_NAMES))}} 
+            
+        else: 
+            self.checkLabels = {'TIME': {self.TIME_LABELS[i] : default.time_columns[i] for i in range(len(self.TIME_LABELS))}, 
+            'AGENTS': {self.AGENT_NAMES[i] + self.AGENT_DATA[k] : -1 for k in range(len(self.AGENT_DATA)) for i in range(len(self.AGENT_NAMES))}} 
+        print(self.checkLabels)
 
         
 
