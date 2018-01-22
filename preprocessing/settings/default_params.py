214f2c1a77ec7a3e6a0e643ed6fcf3bd446e22fe
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-01-22 18:29:00 +0100

    growing list of defaut parameters :)

---
+++ preprocessing/settings/default_params.py
@@ -0,0 +1,9 @@
+time_labels = ['time', 'frames']
+time_columns = [2, 1]
+
+agent_names = ['agent0', 'agent1']
+agent_specifications = ['_x', '_y', '_angle']
+agent_columns = [5,6,7,10,11,13]
+
+tmp_file = 'tmp.csv'
+csv_delim = ';'
