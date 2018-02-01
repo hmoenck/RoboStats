ca19d93d5db3355f49330bcc35eed7cdb718bca9
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-02-01 18:22:51 +0100

    now uses default params for reading csv

--- preprocessing/data_processing/basic_stats.py
+++ preprocessing/data_processing/basic_stats.py
@@ -1,17 +1,25 @@
 import numpy as np
 import pandas as pd
 from collections import Counter
+import settings.default_params as default
+
+
+# when calculating speed or velocity check for  long sequences of zeros, this might indicate measure problems. suggest to omit the respective frames
 
 
 def stats_and_save(filename, info): 
 
-    df = pd.read_csv(filename, header = 0, sep = ',')
+    INDIVIDUAL_STATS = ['_vx', '_vy', '_speed']
+    COLLECTIVE_STATS = ['_dist']
+
+    df = pd.read_csv(filename, header = 0, sep = default.csv_delim)
     df_new = df
     
     # calculate speed and distance
     #FPS = np.mean([len(list(group)) for key, group in groupby(df['time'].values)]) #count how often the same time entry appears to find fps
     c = Counter(df['time'].values)
     FPS = np.mean(np.array(list(c.values())))
+    print('FPS', FPS)
 
     
     DT  = 1. / FPS #timedelta is time per frame 
@@ -32,7 +40,7 @@ def stats_and_save(filename, info):
     # slice in space 
     df_new = space_slicer(df_new, info['agent_names'], info['x_min'], info['x_max'], info['y_min'], info['y_max'])
     
-    return df_new
+    return df_new, INDIVIDUAL_STATS, COLLECTIVE_STATS
     #save to csv
     #df_new.to_csv('first_stats.csv')
 
@@ -57,7 +65,7 @@ def space_slicer(df, agent_names, x_min, x_max, y_min, y_max):
     return df_new 
     
 def velocity(x, dt): 
-    xx = np.gradient(x, dt)
+    xx = np.gradient(x) / float(dt)
     return xx
     
 def speed(vx, vy): 
@@ -68,11 +76,11 @@ def distance(x0, y0, x1, y1):
     d = np.sqrt((x0 - x1)**2 + (y0 - y1)**2)    
     return d
     
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    FILE_NAME = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/tmp.csv'
-    INFO = {'start_time': -1, 'start_frame': 8, 'stop_time': 3000, 'stop_frame':3000, 'duration_time': -1, 'duration_frame':-1,
-            'x_min': 20, 'x_max': 70, 'y_min':20, 'y_max': 80, 'filtered': False, 'agent_names':['agent0', 'agent1']}
+#    FILE_NAME = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/tmp.csv'
+#    INFO = {'start_time': -1, 'start_frame': 8, 'stop_time': 3000, 'stop_frame':3000, 'duration_time': -1, 'duration_frame':-1,
+#            'x_min': 20, 'x_max': 70, 'y_min':20, 'y_max': 80, 'filtered': False, 'agent_names':['agent0', 'agent1']}
 
-    stats_and_save(FILE_NAME, INFO)
+#    stats_and_save(FILE_NAME, INFO)
     
