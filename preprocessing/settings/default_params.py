53a657ec1ba8eed53153edade6d3ad3f8af22850
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-02-01 18:21:30 +0100

    oved column settings to dict_data.json, still contains some absolute paths

--- preprocessing/settings/default_params.py
+++ preprocessing/settings/default_params.py
@@ -1,11 +1,19 @@
-time_labels = ['time', 'frames']
-time_columns = [2, 1]
+#time_labels = ['time', 'frames']
+#time_columns = [2, 1]
 
-agent_names = ['agent0', 'agent1']
-agent_specifications = ['_x', '_y', '_angle']
-agent_columns = [5,6,7,10,11,13]
+#agent_names = ['agent0', 'agent1']
+#agent_specifications = ['_x', '_y', '_angle']
+#agent_columns = [6,7,8, 12,13,14]
 
 tmp_file = 'tmp.csv'
-csv_delim = ','
+csv_delim = ';'
 
-time_format = "'%a %b %d %H:%M:%S %Y'"
+# this should be csv_delim_read and csv_delim_write 
+
+time_format = "%a %b %d %H:%M:%S %Y"
+
+params = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/settings/dict_data.json'
+
+results = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/results/'
+
+info_file = 'info.csv'
