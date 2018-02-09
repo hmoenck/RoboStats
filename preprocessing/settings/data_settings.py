1bea79585d66005ee6f13a82b6167fb03f9d4efa
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-02-09 10:53:06 +0100

    handle timestamp function now tries different datetime formats (german and us)

--- preprocessing/settings/data_settings.py
+++ preprocessing/settings/data_settings.py
@@ -1,18 +1,29 @@
 from datetime import datetime
 import locale
-import settings.default_params as default
+import json
+#import settings.default_params as default
+#date_format_file = '/date_formats.json'
 
-def handle_timestamp(timestamp, time_format): 
+
+def handle_timestamp(timestamp, time_format, date_format_file): 
     print(timestamp)
     if time_format == 'dt':
-        t = handle_datetime(timestamp)
+        t = handle_datetime(timestamp, date_format_file)
     elif time_format == 'ms':
         t = float(timestamp)/1000.
     elif time_format == 's': 
         t = float(timestamp)
     return t
     
-def handle_datetime(timestamp, form = default.time_format, loc = 'en_US.utf8' ): 
-    locale.setlocale(locale.LC_ALL, loc)
-    dt = datetime.strptime(timestamp, form)
+def handle_datetime(timestamp, date_format_file): 
+    
+    date_formats = json.load(open(date_format_file))
+
+    for loc, form in date_formats.items(): 
+        try: 
+            locale.setlocale(locale.LC_ALL, loc)
+            dt = datetime.strptime(timestamp, form)
+        except ValueError:
+            pass
+        
     return dt
