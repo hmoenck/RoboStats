107469ab4b319d0f02f72a69f3263f5719bf091d
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-02-09 10:53:49 +0100

    remove unnecessary varaibles and absolute paths

--- preprocessing/settings/default_params.py
+++ preprocessing/settings/default_params.py
@@ -1,19 +1,22 @@
-#time_labels = ['time', 'frames']
-#time_columns = [2, 1]
 
-#agent_names = ['agent0', 'agent1']
-#agent_specifications = ['_x', '_y', '_angle']
-#agent_columns = [6,7,8, 12,13,14]
 
 tmp_file = 'tmp.csv'
-csv_delim = ';'
+#csv_delim_read = ';'
+#skip_rows_read = 4
 
-# this should be csv_delim_read and csv_delim_write 
+csv_delim = ','
 
-time_format = "%a %b %d %H:%M:%S %Y"
 
-params = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/settings/dict_data.json'
+#time_format = "%a %b %d %H:%M:%S %Y"
+
+
+
+
+
+#params = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/settings/dict_data.json'
 
 results = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/results/'
+
+#csv_info = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/settings/csv_info.json'
 
 info_file = 'info.csv'
