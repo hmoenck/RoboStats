63f5e6baf066f4020ca63c501823b95fe1a24b30
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-02-20 20:10:28 +0100

    if the values of selected time column dont match the specified dateformat the function handle_timestamp will now return none

--- preprocessing/settings/data_settings.py
+++ preprocessing/settings/data_settings.py
@@ -1,21 +1,39 @@
 from datetime import datetime
 import locale
 import json
-#import settings.default_params as default
-#date_format_file = '/date_formats.json'
+import numpy as np
 
 
-def handle_timestamp(timestamp, time_format, date_format_file): 
-    print(timestamp)
+
+def handle_timestamp(timestamp, time_format, date_format_file, rounding = False): 
+    ''' This function takes a string value from the selected time column of the original file 
+    and tries to convert it to either datetime format or a float value. If this is not possible
+    None is returned which then triggers an error message in the window'''
+     
     if time_format == 'dt':
         t = handle_datetime(timestamp, date_format_file)
+        if t == None: 
+            return None
+            
     elif time_format == 'ms':
-        t = float(timestamp)/1000.
+        try:
+            t = float(timestamp)
+        except ValueError: 
+            return None
+        if rounding: t = np.round(t, 2)
+        
     elif time_format == 's': 
-        t = float(timestamp)
+        try: 
+            t = float(timestamp)
+        except ValueError: 
+            return None
+        if rounding: t = np.round(t, 2)
+        
     return t
     
 def handle_datetime(timestamp, date_format_file): 
+    ''' this function tries to convert a given string to datetime format using default formats 
+    specified in the settings file'''
     
     date_formats = json.load(open(date_format_file))
 
@@ -23,7 +41,7 @@ def handle_datetime(timestamp, date_format_file):
         try: 
             locale.setlocale(locale.LC_ALL, loc)
             dt = datetime.strptime(timestamp, form)
-        except ValueError:
-            pass
+        except TypeError:
+            return None
         
     return dt
