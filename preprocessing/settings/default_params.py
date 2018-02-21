7e25787f30f3e5c4c95d0bbf5186a1759f292fc3
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2018-02-21 14:15:40 +0100

    now unnecessary

--- preprocessing/settings/default_params.py
+++ preprocessing/settings/default_params.py
@@ -1,10 +1,12 @@
 
 
-tmp_file = 'tmp.csv'
+#tmp_file = 'tmp.csv'
+#info_file = 'info.csv'
+
 #csv_delim_read = ';'
 #skip_rows_read = 4
 
-csv_delim = ','
+#csv_delim = ','
 
 
 #time_format = "%a %b %d %H:%M:%S %Y"
@@ -19,4 +21,4 @@ csv_delim = ','
 
 #csv_info = '/home/claudia/Dokumente/Uni/lab_rotation_FU/pyQt/preprocessing/settings/csv_info.json'
 
-info_file = 'info.csv'
+
